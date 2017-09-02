eacb5f3504f643948bb3b03e00bb205df8d1c2fb
DanielaTup <daniela.tupiza95@gmail.com>
2017-09-02 16:25:31 -0500

    Mi primer commit

---
+++ hello.py
@@ -0,0 +1,10 @@
+fruits=['apple','orange','banana']
+
+def fruitsNames(name):
+    print ("Hi! " + name)
+    for fruit in fruits:
+        if fruit=='apple':
+            print('I love apples')
+        print('Hello!! I am a ' + fruit)
+
+fruitsNames('Dani')
